6a79930247990caa48dad8bbe0e107236a500a6b
Mathe23 <149236692+Mathe23@users.noreply.github.com>
2024-09-16 04:49:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("display").onclick = function(){
+    document.getElementById("language").textContent = "Java, JavaScript, Html and CSS";
+}
+
+document.getElementById("bio-info").onclick = function(){
+    document.getElementById("bio-details").textContent = "I was born and raised in South Africa, Limpopo Phalaborwa in 2002 January, i speak Xitsonga as my home language and English as second additional language. I am currently not married and i don't have any criminal record, the highest qualification i have is grade 12.";
+}
+
